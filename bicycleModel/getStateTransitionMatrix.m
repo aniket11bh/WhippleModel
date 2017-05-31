698cacabeedf3a65206ddece92aa953838f1a3d9
Aniket Bhushan <aniket2701bhushan@gmail.com>
2017-05-31 10:07:52 +0530

    Adding options in state Transition matrix for different types of gain
    Signed-off-by: Aniket Bhushan <aniket2701bhushan@gmail.com>

--- bicycleModel/getStateTransitionMatrix.m
+++ bicycleModel/getStateTransitionMatrix.m
@@ -1,6 +1,7 @@
-function [A] = getStateTransitionMatrix( v, B )
+function [A] = getStateTransitionMatrix( v, B, type )
 %GET_AB_MATRICES returns A and B matrices
 %   input: v speed of the bicycle
+%   type: "0, 1, 2, 3" : A :: A - B*K_l :: A-B*K*s :: A-B*Ks-B*Kl"
 
 global bic g;
 
@@ -12,16 +13,39 @@ S2 = -bic.invM*C;
 A    = [ 0 0 1 0 ;
              0 0 0 1 ;
              S1  S2 ];
-         
-K_lean = zeros(1,4);
-K_lean(1) = 7843.20023833066;
-K_lean(3) = 424.726009043642;
 
-K_steer = zeros(1,4);
-K_steer(2) = 9.46120848195432;
-K_steer(4) = 1.387655359381;
+if type == 0
+    A = A;
+    
+elseif type  == 1
+    K_lean = zeros(1,4);
+    K_lean(1) = 7843.20023833066;
+    K_lean(3) = 424.726009043642;
+    
+    A = A - B(:,1)*K_lean;
+    
+elseif type == 2
+        
+    K_steer = zeros(1,4);
+    K_steer(2) = 9.46120848195432;
+    K_steer(4) = 1.387655359381;
+    
+    A = A - B(:,2)*K_steer;
 
-A = A - B(:,1)*K_lean - B(:,2)*K_steer;
+elseif type == 3
+    K_lean = zeros(1,4);
+    K_lean(1) = 7843.20023833066;
+    K_lean(3) = 424.726009043642;
+
+    K_steer = zeros(1,4);
+    K_steer(2) = 9.46120848195432;
+    K_steer(4) = 1.387655359381;
+
+    A = A - B(:,1)*K_lean - B(:,2)*K_steer;
+else
+    fprintf('Wrong input');
+end
+
 
 end
 
